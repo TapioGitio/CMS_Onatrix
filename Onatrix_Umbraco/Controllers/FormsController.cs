996984fefa2f76303e49c59def60245384a7ff15
Janne <tapioninetytwo@live.se>
2025-10-30 14:55:58 +0100

    styled the form, added controller, VM, and submissions to umbraco

---
+++ Onatrix_Umbraco/Controllers/FormsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onatrix_Umbraco.ViewModels;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Onatrix_Umbraco.Controllers;
+
+public class FormsController : SurfaceController
+{
+    public FormsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    public IActionResult HandleCallbackForm(CallbackFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+
+        // Work with form data here
+
+        return RedirectToCurrentUmbracoPage();
+    }
+}
